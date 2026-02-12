0f877383ab845efcfee5e05ecfbd6f8bc7c1a216
Rodrigo Caldas <rocaldas25pi@epms.pt>
2026-02-12 11:37:21 +0000

    Update: sistema de pontuação feito

--- logica.py
+++ logica.py
@@ -2,9 +2,31 @@ import json
 
 def carregar_perguntas(ficheiro_com_perguntas):
     try:
-        with open(nome_ficheiro, "r", encoding="utf-8") as f:
+        with open(ficheiro_com_perguntas, "r", encoding="utf-8") as f:
             return json.load(f)
 
     except (FileNotFoundError, json.JSONDecodeError):
         print("Houve um erro no ficheiro")
         return []
+
+
+def guardar_info(nome, pontos, ficheiro):
+
+    with open(ficheiro, "r", encoding="utf-8") as f:
+        lista_carregada = json.load(f)
+
+    informacao = {
+
+        nome : pontos
+    }
+
+    lista_carregada[nome] = pontos
+    try:
+        with open(ficheiro, "w", encoding="utf-8") as f:
+            json.dump(lista_carregada, f)
+
+    except (FileNotFoundError, json.JSONDecodeError):
+        print("Houve um erro no ficheiro")
+
+
+guardar_info("teste", 25, "pontuacao.json")
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 
+import json
 nome = input("digite seu nome: ")
 
 
-
+from logica import guardar_info, carregar_perguntas
 from menu import mostrar_menu
+testanto_pontuação = int(input("Escreva um numero pra pontuação: "))
+guardar_info(nome, testanto_pontuação, "pontuacao.json")  
+"""
 while True:
     escolhi = mostrar_menu()
 
@@ -14,11 +18,13 @@ while True:
         print(f"Então vamos pra pontuação {nome}")
     
     elif escolhi == "3":
+        print(f"Então vamos para as regras, {nome}")
+    
+    elif escolhi == "4":
         print(f"OK Adeus {nome}")
         break
     else:
         input("O que voce escreveu não corresponde com nenhuma das opções da aplicação, tente novamente")
         
         
-
-          
+       """
